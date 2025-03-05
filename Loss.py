6e69bef6bdb0e5779c350735818cf90f735be67e
jjimmanuel <99039852+jjimmanuel@users.noreply.github.com>
2025-03-05 13:15:34 -0500

    Update Loss.py
    Generic Loss calculation between true tensor and pred vector. Need to better define batching for correct loss calculation.

--- Loss.py
+++ Loss.py
@@ -1,7 +1,25 @@
-class TotalFeatureLoss():
+class NodeFeatureLoss():
   def __init__(self, loss_fn=nn.MSELoss()):
     self.loss_fn = loss_fn
 
-  def feature_loss(self, pred, true):
-    total_loss = nn.MSELoss()(pred, true)
+  def feature_loss(self, node_pred, node_true):
+    node_loss = nn.MSELoss()(node_pred, node_true)
     return node_loss
+
+
+class EdgeFeatureLoss():
+  def __init__(self, loss_fn=nn.MSELoss()):
+    self.loss_fn = loss_fn
+
+  def feature_loss(self, edge_pred, edge_true):
+    edge_loss = nn.MSELoss()(edge_pred, edge_true)
+    return edge_loss
+
+
+class TotalFeatureLoss():
+  def __init__(self, edge_loss, node_loss):
+    self.edge_loss = edge_loss
+    self.node_loss = node_loss
+  
+  def total_loss(self):
+    return self.edge_loss + self.node_loss
